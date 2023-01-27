4f13f7849eeb22aeda86ced1d21a1439a4ee2ab2
Elias Rodriguez <elias.rodriguez@southworks.com>
2023-01-27 16:29:24 -0300

    Working without sessionId

--- Poc ServiceBus ExactlyOnce/Poc ServiceBus ExactlyOnce/PocServiceBusTrigger.cs
+++ Poc ServiceBus ExactlyOnce/Poc ServiceBus ExactlyOnce/PocServiceBusTrigger.cs
@@ -3,6 +3,7 @@ using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Threading.Tasks;
 
 public class PocServiceBusTrigger
@@ -23,20 +24,25 @@ public class PocServiceBusTrigger
 
         for (int i = 0; i < 10; i++)
         {
-            var serviceBusMessage = new ServiceBusMessage(i.ToString());
+            var serviceBusMessage = new ServiceBusMessage();
             serviceBusMessage.MessageId = MESSAGEID;
-            //serviceBusMessage.SessionId = MESSAGEID;
-            serviceBusMessage.SessionId = i.ToString();
+
+            var random = Random.Shared.Next(100, 1000).ToString();
+            var serviceBusRandomMessage = new ServiceBusMessage(random);
+            serviceBusRandomMessage.MessageId = random;
+
             await sender.SendMessageAsync(serviceBusMessage);
-            logger.LogWarning("SENT SAME MESSAGEID {Id} with sessionId {SessionId}", serviceBusMessage.MessageId, serviceBusMessage.SessionId);
+            await sender.SendMessageAsync(serviceBusRandomMessage);
+            logger.LogWarning("SENT with SAME messageId {MessageId}", MESSAGEID);
+            logger.LogWarning("SENT with RANDOM messageId {MessageId}", random);
         }
     }
 
     [FunctionName(nameof(PocServiceBusTriggerAsync))]
     public static void PocServiceBusTriggerAsync(
-        [ServiceBusTrigger("%ServiceBusQueueName%", IsSessionsEnabled = true)] ServiceBusReceivedMessage message, ILogger logger)
+        [ServiceBusTrigger("%ServiceBusQueueName%")] ServiceBusReceivedMessage message, ILogger logger)
     {
-        logger.LogError("RECIEVED message {Message} with sessionId {SessionId}", message.Body, message.SessionId);
+        logger.LogError("RECIEVED message {MessageId}", message.MessageId);
     }
 }
 
