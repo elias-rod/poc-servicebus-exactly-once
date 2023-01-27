8514d33e58f73e0d4ab05df091d9ab0c3ac1ad87
Elias Rodriguez <elias.rodriguez@southworks.com>
2023-01-27 16:38:25 -0300

    Minor fixes

--- Poc ServiceBus ExactlyOnce/Poc ServiceBus ExactlyOnce/PocServiceBusTrigger.cs
+++ Poc ServiceBus ExactlyOnce/Poc ServiceBus ExactlyOnce/PocServiceBusTrigger.cs
@@ -19,24 +19,23 @@ public class PocServiceBusTrigger
     {
         var serviceBusClient = new ServiceBusClient(_config.GetValue<string>("AzureWebJobsServiceBus"));
         await using var sender = serviceBusClient.CreateSender(_config.GetValue<string>("ServiceBusQueueName"));
-        const string MESSAGEID = "1";
-
+        
         for (int i = 0; i < 10; i++)
         {
             var serviceBusMessage = new ServiceBusMessage(i.ToString());
-            serviceBusMessage.MessageId = MESSAGEID;
-            //serviceBusMessage.SessionId = MESSAGEID;
-            serviceBusMessage.SessionId = i.ToString();
+            serviceBusMessage.MessageId = "MessageId";
+            serviceBusMessage.SessionId = "SAME";
+            //serviceBusMessage.SessionId = i.ToString();
             await sender.SendMessageAsync(serviceBusMessage);
-            logger.LogWarning("SENT SAME MESSAGEID {Id} with sessionId {SessionId}", serviceBusMessage.MessageId, serviceBusMessage.SessionId);
+            logger.LogWarning("SENT message {ServiceBusMessage} with sessionId {sessionId}", serviceBusMessage.Body, serviceBusMessage.SessionId);
         }
     }
 
     [FunctionName(nameof(PocServiceBusTriggerAsync))]
     public static void PocServiceBusTriggerAsync(
-        [ServiceBusTrigger("%ServiceBusQueueName%", IsSessionsEnabled = true)] ServiceBusReceivedMessage message, ILogger logger)
+        [ServiceBusTrigger("%ServiceBusQueueName%", IsSessionsEnabled = true)] ServiceBusReceivedMessage serviceBusReceivedMessage, ILogger logger)
     {
-        logger.LogError("RECIEVED message {Message} with sessionId {SessionId}", message.Body, message.SessionId);
+        logger.LogError("RECIEVED message {Message} with sessionId {SessionId}", serviceBusReceivedMessage.Body, serviceBusReceivedMessage.SessionId);
     }
 }
 
