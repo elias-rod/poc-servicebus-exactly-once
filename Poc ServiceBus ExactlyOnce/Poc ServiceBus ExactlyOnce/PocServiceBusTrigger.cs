30ac1fc4a6bbd39618d1287a05a5be3f90c2ae65
Elias Rodriguez <elias.rodriguez@southworks.com>
2023-01-27 17:05:55 -0300

    Minor fixes

--- Poc ServiceBus ExactlyOnce/Poc ServiceBus ExactlyOnce/PocServiceBusTrigger.cs
+++ Poc ServiceBus ExactlyOnce/Poc ServiceBus ExactlyOnce/PocServiceBusTrigger.cs
@@ -1,4 +1,3 @@
-using Azure.Identity;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
@@ -6,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
+//Create queue with Duplicate detection
 public class PocServiceBusTrigger
 {
     private readonly IConfiguration _config;
@@ -16,25 +16,20 @@ public class PocServiceBusTrigger
 
     [FunctionName(nameof(PocTimerTrigger))]
     public async Task PocTimerTrigger(
-        [TimerTrigger("* * 17 * * *", RunOnStartup = true)] TimerInfo timerInfo, ILogger logger)
+        [TimerTrigger("* * 17 1 * *", RunOnStartup = true)] TimerInfo timerInfo, ILogger logger)
     {
         var serviceBusClient = new ServiceBusClient(_config.GetValue<string>("AzureWebJobsServiceBus"));
         await using var sender = serviceBusClient.CreateSender(_config.GetValue<string>("ServiceBusQueueName"));
-        const string MESSAGEID = "1";
 
         for (int i = 0; i < 10; i++)
         {
-            var serviceBusMessage = new ServiceBusMessage();
-            serviceBusMessage.MessageId = MESSAGEID;
-
-            var random = Random.Shared.Next(100, 1000).ToString();
-            var serviceBusRandomMessage = new ServiceBusMessage(random);
-            serviceBusRandomMessage.MessageId = random;
+            var serviceBusMessage = new ServiceBusMessage { MessageId = "SameMessageId" };
+            var serviceBusRandomMessage = new ServiceBusMessage { MessageId = Random.Shared.Next(100, 1000).ToString() };
 
             await sender.SendMessageAsync(serviceBusMessage);
+            logger.LogWarning("SENT with SAME messageId {MessageId}", serviceBusMessage.MessageId);
             await sender.SendMessageAsync(serviceBusRandomMessage);
-            logger.LogWarning("SENT with SAME messageId {MessageId}", MESSAGEID);
-            logger.LogWarning("SENT with RANDOM messageId {MessageId}", random);
+            logger.LogWarning("SENT with RANDOM messageId {MessageId}", serviceBusRandomMessage.MessageId);
         }
     }
 
