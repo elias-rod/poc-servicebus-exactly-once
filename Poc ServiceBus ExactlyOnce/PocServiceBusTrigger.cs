de1e360ec2de74b1d03b692b68b1e8794d35c62c
Elias Rodriguez <elias.rodriguez@southworks.com>
2023-01-30 12:07:49 -0300

    HttpTrigger

--- Poc ServiceBus ExactlyOnce/PocServiceBusTrigger.cs
+++ Poc ServiceBus ExactlyOnce/PocServiceBusTrigger.cs
@@ -1,5 +1,7 @@
 using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
@@ -12,22 +14,19 @@ public class PocServiceBusTrigger
         _config = config;
     }
 
-    [FunctionName(nameof(PocTimerTrigger))]
-    public async Task PocTimerTrigger(
+    [FunctionName(nameof(PocTimerTriggerAsync))]
+    public async Task PocTimerTriggerAsync(
         [TimerTrigger("* * 17 1 * *", RunOnStartup = true)] TimerInfo timerInfo, ILogger logger)
     {
-        var serviceBusClient = new ServiceBusClient(_config.GetValue<string>("AzureWebJobsServiceBus"));
-        await using var sender = serviceBusClient.CreateSender(_config.GetValue<string>("ServiceBusQueueName"));
-        
-        for (int i = 0; i < 10; i++)
-        {
-            var serviceBusMessage = new ServiceBusMessage(i.ToString());
-            serviceBusMessage.MessageId = "MessageId";
-            serviceBusMessage.SessionId = "SAME";
-            //serviceBusMessage.SessionId = i.ToString();//Uncomment this and comment above to test with variable session id
-            await sender.SendMessageAsync(serviceBusMessage);
-            logger.LogWarning("SENT message {ServiceBusMessage} with sessionId {sessionId}", serviceBusMessage.Body, serviceBusMessage.SessionId);
-        }
+        await SendMessagesAsync(logger);
+    }
+
+    [FunctionName(nameof(PocHttpTriggerAsync))]
+    public async Task PocHttpTriggerAsync(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+    ILogger logger)
+    {
+        await SendMessagesAsync(logger);
     }
 
     [FunctionName(nameof(PocServiceBusTriggerAsync))]
@@ -35,6 +34,22 @@ public class PocServiceBusTrigger
         [ServiceBusTrigger("%ServiceBusQueueName%", IsSessionsEnabled = true)] ServiceBusReceivedMessage serviceBusReceivedMessage, ILogger logger)
     {
         logger.LogError("RECIEVED message {Message} with sessionId {SessionId}", serviceBusReceivedMessage.Body, serviceBusReceivedMessage.SessionId);
+    }
+
+    private async Task SendMessagesAsync(ILogger logger)
+    {
+        var serviceBusClient = new ServiceBusClient(_config.GetValue<string>("AzureWebJobsServiceBus"));
+        var sender = serviceBusClient.CreateSender(_config.GetValue<string>("ServiceBusQueueName"));
+
+        for (int i = 0; i < _config.GetValue<int>("NumberOfMessages"); i++)
+        {
+            var serviceBusMessage = new ServiceBusMessage(i.ToString());
+            serviceBusMessage.MessageId = "MessageId";
+            //serviceBusMessage.SessionId = "SAME";
+            serviceBusMessage.SessionId = i.ToString();//Uncomment this and comment above to test with variable session id
+            await sender.SendMessageAsync(serviceBusMessage);
+            logger.LogWarning("SENT message {ServiceBusMessage} with sessionId {sessionId}", serviceBusMessage.Body, serviceBusMessage.SessionId);
+        }
     }
 }
 
